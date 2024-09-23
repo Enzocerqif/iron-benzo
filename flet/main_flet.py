98c0409c4eb1e1a9b7cf8688854b73f17379f62d
Kowass <felipe.sousa62@aluno.ifce.edu.br>
2024-09-23 14:10:59 -0300

    Adicionando função de cadastrar plano e corrigindo erros

--- flet/main_flet.py
+++ flet/main_flet.py
@@ -5,6 +5,8 @@ from search_info_flet import checar_informacoes
 from update_data_flet import atualizar_dados
 from update_plan_flet import atualizar_plano
 from add_ficha_treino_flet import cadastrar_ficha_treino, ver_fichas
+from add_plano_db_flet import cadastrar_plano
+
 
 def main(page: ft.Page):
     page.title = "Sistema da Academia Iron Benzo"
@@ -21,8 +23,9 @@ def main(page: ft.Page):
                     button("Cadastrar ficha de treino", on_click=show_cadastrar_ficha),
                     button("Ver fichas de treino", on_click=show_ver_fichas),
                     button("Todos os alunos", on_click=checar_informacoes_click),
+                    button("Cadastrar novo plano", on_click=cadastrar_plano_click),
                     button("Atualizar dados", on_click=atualizar_dados_click),
-                    button("Atualizar plano", on_click=atualizar_plano_click),
+                    button("Atualizar plano do aluno", on_click=atualizar_plano_click),
                     button("Sair", on_click=fechar_programa_click),
                 ],
                 alignment=ft.MainAxisAlignment.CENTER,
@@ -40,6 +43,12 @@ def main(page: ft.Page):
     def checar_informacoes_click(e):
         page.controls.clear()
         checar_informacoes(page)
+        add_back_button()
+        page.update()
+
+    def cadastrar_plano_click(e):
+        page.controls.clear()
+        cadastrar_plano(page)
         add_back_button()
         page.update()
 
